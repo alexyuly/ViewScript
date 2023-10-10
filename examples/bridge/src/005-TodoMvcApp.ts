e34f485223d055152a342326d986e7c53922be86
Alex Yuly <alexyuly2@gmail.com>
2023-10-10 12:41:00 -0700

    WIP on 005-TodoMvcApp example

---
+++ examples/bridge/src/005-TodoMvcApp.ts
@@ -0,0 +1,120 @@
+// TODO
+// This is a work in progress...
+
+import {
+  browser,
+  condition,
+  element,
+  render,
+  stream,
+  text,
+  view,
+} from "viewscript-bridge";
+
+// TODO Support models, and define the TodoItem model.
+
+const TodoListItem = view(
+  {
+    content: text(),
+    done: condition(),
+    markAsDone: stream(),
+  },
+  ({ content, done, markAsDone }) =>
+    element("li", {
+      click: markAsDone,
+      cursor: "pointer",
+      margin: "8px 0",
+      display: "flex",
+      "flex-direction": "row",
+      "align-items": "center",
+      gap: "4px",
+      content: [
+        element("input", {
+          type: "checkbox",
+          checked: done,
+        }),
+        element("span", {
+          content,
+        }),
+        // TODO Make this work:
+        // content,
+      ],
+    })
+);
+
+render(
+  element("section", {
+    position: "fixed",
+    width: "100%",
+    height: "100%",
+    top: 0,
+    left: 0,
+    "box-sizing": "border-box",
+    padding: "24px",
+    display: "flex",
+    "flex-direction": "column",
+    "align-items": "flex-start",
+    gap: "8px",
+    font: "medium sans-serif",
+    content: [
+      element("h1", {
+        content: "TodoMVC",
+        margin: 0,
+      }),
+      element("hr", {
+        width: "100%",
+      }),
+      element("form", {
+        // TODO Handle form submit events.
+        display: "flex",
+        "flex-direction": "column",
+        "align-items": "flex-start",
+        gap: "8px",
+        content: [
+          element("label", {
+            display: "flex",
+            "flex-direction": "column",
+            "align-items": "flex-start",
+            gap: "8px",
+            content: [
+              element("span", {
+                content: "Create a new todo:",
+              }),
+              element("input", {
+                type: "text",
+                font: "inherit",
+                placeholder: "Enter todo text here...",
+              }),
+            ],
+          }),
+          element("button", {
+            type: "submit",
+            font: "inherit",
+            "font-weight": "bold",
+            content: "Add todo",
+          }),
+        ],
+      }),
+      element("hr", {
+        width: "100%",
+      }),
+      element("ul", {
+        margin: 0,
+        "padding-inline": "16px 0",
+        // TODO Iterate over a collection, instead of hardcoding elements:
+        content: [
+          element(TodoListItem, {
+            content: "Todo List Item 1",
+            done: true,
+            markAsDone: browser.console.log("You clicked a todo checkbox!"),
+          }),
+          element(TodoListItem, {
+            content: "Todo List Item 2",
+            done: false,
+            markAsDone: browser.console.log("You clicked a todo checkbox!"),
+          }),
+        ],
+      }),
+    ],
+  })
+);
