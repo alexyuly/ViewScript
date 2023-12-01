5544362177a6187c003c250a4d9a0af594101572
Alex Yuly <alexyuly2@gmail.com>
2023-12-01 09:33:09 -0800

    wip

--- examples/bridge/src/004-CounterWithIncrementAndReset.ts
+++ examples/bridge/src/004-CounterWithIncrementAndReset.ts
@@ -1,1 +1,83 @@
 // TODO
+
+const FancyButton = view<{
+  click: Output;
+  disabled: boolean;
+  content: string;
+}>(({ click, disabled, content }) => {
+  const hovered = store(false);
+
+  render("<button>", {
+    click,
+    pointerLeave: hovered.setTo(false),
+    pointerOver: hovered.setTo(true),
+    disabled,
+    "align-items": "center",
+    background: when(disabled.not.and(hovered)).then("lightgray").else("lightgreen"),
+    color: "crimson",
+    cursor: "pointer",
+    display: "flex",
+    "font-weight": "bold",
+    height: "100px",
+    "justify-content": "center",
+    width: "100px",
+    content,
+  });
+});
+
+// FancyButton = view {
+//   click: output
+//   disabled: boolean
+//   content: string
+
+//   hovered = false
+
+//   render <button> {
+//     click
+//     pointerLeave = hovered.setTo false
+//     pointerOver = hovered.setTo true
+//     disabled
+//     align-items = "center"
+//     background = if disabled.not.(and hovered) then "lightgray" else "lightgreen"
+//     color = "crimson"
+//     cursor = "pointer"
+//     display = "flex"
+//     font-weight = "bold"
+//     height = "100px"
+//     justify-content = "center"
+//     width = "100px"
+//     content
+//   }
+// }
+
+// render {
+//   clicks = 0
+
+//   render <section> {
+//     align-items = "center"
+//     border = "2px dashed red"
+//     display = "flex"
+//     flex-direction = "column"
+//     gap = "16px"
+//     height = "200px"
+//     justify-content = "center"
+//     margin = "24px"
+//     padding = "12px"
+//     width = "200px"
+//     content = [
+//       FancyButton {
+//         click = clicks.add 1
+//         disabled = clicks.isAtLeast 10
+//         content = "Increment"
+//       }
+//       FancyButton {
+//         click = clicks.setTo 0
+//         disabled = clicks.is 0
+//         content = "Reset"
+//       }
+//       <span> {
+//         content = clicks
+//       }
+//     ]
+//   }
+// }
